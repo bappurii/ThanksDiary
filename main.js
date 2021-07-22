7e2a22681595915e9725b8676c5161c367f6c53a
bappurii <bappul_tt@naver.com>
2021-07-22 21:30:10 +0900

    add delete & make module

--- main.js
+++ main.js
@@ -1,54 +1,161 @@
-const fs = require('fs');
 const url = require('url');
 const http = require('http');
 const qs = require('querystring');
-const path=require('path')
-const sanitizeHtml = require('sanitize-html');
 const mysql= require('mysql');
+const tpl = require('./lib/template');
+
 const cn = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : 'qkqvnfl2',
-    database : 'diary'
+    database : 'diary1'
 });
 cn.connect();
 
 
-//ctg table
-
-cn.query('select * from ctg', 
-    function (error, results) {
-        if (error) throw error;
-        let ctg_list='';
-        for (let i=0; i<results.length; i++){
-            ctg_list = ctg_list + `<ul><li><a href="/ctg${results[i].id
-            }">${results[i].category}</a></li></ul>`;
-        }
-        console.log(ctg_list);
-    }
-)
 
 
 
-    
+
+
 
 
 let server =http.createServer(function (req, res) {
     let pathname =url.parse(req.url, true).pathname;
     let queryData = url.parse(req.url, true).query;
 
+
+    //ctg table
+    cn.query('select * from ctg', 
+    function (error, results) {
+        if (error) throw error;
+        let ctg_list='';
+        for (let i=0; i<results.length; i++){
+            ctg_list = ctg_list + `<ul><li><a href="/?ctg_id=${results[i].id
+            }">${results[i].category}</a></li></ul>`;
+        }
+        console.log(ctg_list);
+    });
+
     //date
-    cn.query(`select date_format(date, "%y-%m-%d") as date, content from ${pathname.substring(1)}`, 
+    cn.query(`select id, ctg_id, date_format(date, "%y-%m-%d") as date, content from total where ctg_id=${pathname.substring(4)}`, 
         function (error, results, fields) {
             if (error) throw error;
-            
             let date='';
             for (let i=0; i<results.length; i++){
-                date=date + `<ul><li><a href="${pathname}?date=${results[i].date}">${results[i].date}</a></li></ul>`
+                date=date + `<ul><li><a href="/${results[i].id}">${results[i].date}</a></li></ul>`
             };
         }
     )
-})
+
+    //button
+    if (pathname=="/") {
+        button="";
+    } else if(queryData.id){
+        button=`
+            <a href="${pathname}?process=create">create</a>
+            <a href="${pathname}?id=${queryData.id}&process=amend">amend</a>
+            <form action="${pathname}?id=${queryData.id}&process=deleting" method="post">
+                <input type="submit" value="delete">
+            </form>
+        `;
+    } else{
+        button=`
+            <a href="${pathname}?process=create">create</a>
+        `;
+    };
+
+
+    
+
+
+    //content
+    let content='';
+    if (pathname =="/") {
+        content ="Welcome!";
+        tpl.normalRes(ctg_list, date, button, content);
+    } else if(queryData.id){
+        cn.query(`select content from total where id=${queryData.id}`,function (error, results, fields) {
+            if (error) throw error;
+            if (results[0].id){
+                content=results[0].id;
+            }
+        })
+        tpl.normalRes(ctg_list, date, button, content);
+    } else if(queryData.process=="create") {
+        content = `
+        <form action="${pathname}?process=creating" method="post">
+            <p><input type="date" name="new_date"></p>
+            <p><textarea name="new_content" placeholder="content"></textarea></p>
+            <input type="submit" >
+        </form>
+        `;
+        tpl.normalRes(ctg_list, date, button, content);
+    } else if(queryData.process== "creating"){
+        let body="";
+        req.on('data', function (data) {
+            body += data;
+            if (body.length > 1e6){
+                req.cn.destroy();
+            }
+            
+        });
+
+        req.on('end', function () {
+            var post = qs.parse(body);
+            let new_date = post.new_date;
+            let new_content= post.new_content;
+            cn.query(`insert into total (ctg_id, date, content) values (${pathname.substring(4)}, "${new_date}", "${new_content}")`)
+            res.writeHead(302, {Location: `${pathname}`});
+            res.end(tpl.template(ctg_list, date, button, content));
+        });
+
+    } else if(queryData.process=="amend"){
+        content = `
+        <form action="${pathname}?id=${queryData.id}&process=amending" method="post">
+            <p><input type="date" name="new_date" placeholder="date" value="${queryData.date}"></p>
+            <p><textarea name="new_content">${file}</textarea></p>
+            <input type="submit" value="submit">
+        </form>
+        `;
+        tpl.normalRes(ctg_list, date, button, content);
+
+    } else if(queryData.process=="amending"){
+        let body="";
+        req.on('data', function (data) {
+            body += data;
+            if (body.length > 1e6){
+                req.cn.destroy();
+            }
+        });
+        
+        req.on('end', function () {
+            var post = qs.parse(body);
+            let new_date = post.new_date;
+            let new_content= post.new_content;
+            cn.query(`update total set date="${new_date}", content="${new_content}" where id=${queryData.id}`
+
+            )
+            res.writeHead(302, {Location: `${pathname}`});
+            res.end(tpl.template(ctg_list, date, button, content));
+        });
+    } else if (pathname.process="deleting"){
+        var answer = confirm("Are you sure to delete this?");
+        if (answer) {
+            cn.query(`delete from total where id=queryData.id`);
+            res.writeHead(302, {Location: `${pathname}`});
+            res.end(tpl.template(ctg_list, date, button, content));
+        }
+        else {
+            res.writeHead(200, {Location: `${pathname}/?id=${queryData.id}`});
+            res.end(tpl.template(ctg_list, date, button, content));
+        }
+    } else {
+        content = '';
+        tpl.normalRes(ctg_list, date, button, content);
+    }
+});
+
 server.listen(7000); 
 
 
@@ -60,78 +167,31 @@ server.listen(7000);
 //         let pathname =url.parse(req.url, true).pathname;
 //         let queryData = url.parse(req.url, true).query;
 
-//         let date="";
-//         fs.readdir(`./What${pathname}`,"utf8", (err,fileName)=>{
-//             if(fileName){
-//                 fileName.forEach(element => {
-//                 date=date + `<ul><li><a href="${pathname}?date=${element}">${element}</a></li></ul>`;
-//                 })
-//             };
+//         
             
 
 //             fs.readFile(`./What${pathname}/${queryData.date}`,'utf8', (err, file) => {
 
                 
 //                 //date & button
-//                 if (pathname=="/") {
-//                     date="";
-//                     button="";
-//                 } else if(queryData.date){
-//                     button=`
-//                         <a href="${pathname}?process=new">new</a>
-//                         <a href="${pathname}?date=${queryData.date}&process=amend">amend</a>
-//                         <form action="${pathname}?date=${queryData.date}&process=deleting" method="post">
-//                             <input type="submit" value="delete">
-//                         </form>
-//                     `;
-//                 } else{
-//                     button=`
-//                         <a href="${pathname}?process=new">new</a>
-//                     `;
-//                 };
+//                 
 
 //                 //content
 
-//                 function template (ctg_list, date, button, content) {
-//                     return `
-//                     <!DOCTYPE html>
-//                     <html>
-//                     <head>
-//                         <meta charset="UTF-8">
-//                         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-//                         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//                         <title>PositiveDiary</title>
-//                     </head>
-//                     <body>
-//                         <h1><a href="/">Positive Diary</a></h1>
-//                         <nav>
-//                         ${ctg_list}
-//                         ${date}
-//                         ${button}
-//                         </nav>
-//                         ${content}
-//                     </body>
-//                     </html>
-//                     `;
-//                 }
-//                 function normalRes(ctg_list, date, button, content){
-//                     res.writeHead(200);
-//                     res.write(template(ctg_list, date, button, content));
-//                     res.end();
-//                 }
+//                
                     
                 
 
 //                 let content='';
 //                 if (pathname =="/") {
 //                     content ="Welcome!";
-//                     normalRes(ctg_list, date, button, content);
+//                     tpl.normalRes(ctg_list, date, button, content);
 //                 } else if( !queryData.process){
 //                     if(file){
 //                         content=file;
 //                     }
-//                     normalRes(ctg_list, date, button, content);
-//                 } else if(queryData.process=="new") {
+//                     tpl.normalRes(ctg_list, date, button, content);
+//                 } else if(queryData.process=="create") {
 //                     content = `
 //                     <form action="${pathname}?process=creating" method="post">
 //                         <p><input type="date" name="new_date"></p>
@@ -139,7 +199,7 @@ server.listen(7000);
 //                         <input type="submit" >
 //                     </form>
 //                     `;
-//                     normalRes(ctg_list, date, button, content);
+//                     tpl.normalRes(ctg_list, date, button, content);
 //                 } else if(queryData.process== "creating"){
                     
 //                     let body="";
@@ -158,20 +218,20 @@ server.listen(7000);
 //                         fs.writeFile(path.join(__dirname,`./What/${pathname}`,new_date), new_content,"utf8",(err)=>{
 //                         });
 //                         res.writeHead(302, {Location: `${pathname}`});
-//                         res.end(template(ctg_list, date, button, content));
+//                         res.end(tpl.template(ctg_list, date, button, content));
 //                     });
 
                     
 
 //                 } else if(queryData.process=="amend"){
 //                     content = `
-//                     <form action="${pathname}?date=${queryData.date}&process=amending" method="post">
+//                     <form action="${pathname}?id=${queryData.id}&process=amending" method="post">
 //                         <p><input type="date" name="new_date" placeholder="date" value="${queryData.date}"></p>
 //                         <p><textarea name="new_content">${file}</textarea></p>
 //                         <input type="submit" value="submit">
 //                     </form>
 //                     `;
-//                     normalRes(ctg_list, date, button, content);
+//                     tpl.normalRes(ctg_list, date, button, content);
 //                 } else if(queryData.process=="amending"){
 //                     let body="";
 //                     req.on('data', function (data) {
@@ -190,21 +250,28 @@ server.listen(7000);
 //                             fs.writeFile(path.join(__dirname,`./What/${pathname}`,new_date), new_content,"utf8",(err)=>{
 //                                 // let a=`localhost:${server.address().port}${pathname}?date=${new_date}`
 //                                 res.writeHead(302, {Location: `${pathname}`});
-//                                 res.end(template(ctg_list, date, button, content));
+//                                 res.end(tpl.template(ctg_list, date, button, content));
 //                             });
 //                         });
 //                     });
 
 //                 } else if (pathname.process="deleting"){
+//     var answer = window.confirm("Save data?");
+// if (answer) {
+//     //some code
+// }
+// else {
+//     //some code
+// }
 //                         fs.unlink(`./What/${pathname}/${queryData.date}`,(err)=>{
 //                             // let a =`localhost:${server.address().port}${pathname}`
 //                             res.writeHead(302, {Location: `${pathname}`});
-//                             res.end(template(ctg_list, date, button, content));
+//                             res.end(tpl.template(ctg_list, date, button, content));
 //                         });
                         
 //                 } else {
 //                     content = '';
-//                     normalRes(ctg_list, date, button, content);
+//                     tpl.normalRes(ctg_list, date, button, content);
 //                 }
 
                 
