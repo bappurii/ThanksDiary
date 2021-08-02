e1df95c365db36766dba6d19889d2130363b9a36
bappurii <bappul_tt@naver.com>
2021-08-02 22:58:29 +0900

    add ctg CUD in module ctg.js

--- router/ctg.js
+++ router/ctg.js
@@ -9,17 +9,98 @@ const sanitizeHtml = require('sanitize-html');
 // express
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express()
 const router = express.Router()
 
-app.use(bodyParser.urlencoded({ extended: false }))
+router.use(bodyParser.urlencoded({ extended: false }))
 
 
+
+//ctg CUD
+router.get('/ctg_create', function(req,res){
+    content = `
+        <form action="/ctg/ctg_creating" method="post">
+            <p><input type="text" name="new_category" placeholder="new category"></p>
+            <input type="submit" >
+        </form>
+        `;
+    cn.query('select * from ctg', 
+    function (error, ctg_results) {
+        if (error) throw error;
+        let ctg_list =tpl.ctg_list(ctg_results);
+        res.send(tpl.template(ctg_list, '', '', '', content));
+    })
+})
+
+router.post('/ctg_creating', function(req,res){
+    let new_category=req.body.new_category;
+    cn.query(`insert into ctg (category) values ("${new_category}")`,function (err){
+        if (err) throw err;
+        cn.query(`select last_insert_id() as ctg_id from ctg`,function (err, results) {
+            if (err) throw err;
+            cn.query('select * from ctg', 
+            function (error, ctg_results) {
+                let button = tpl.button(req);
+                if (error) throw error;
+                let ctg_list =tpl.ctg_list(ctg_results);
+                res.send(tpl.template(ctg_list, '', '', button, ''));
+            })
+        });
+    });
+})
+
+
+router.get('/:ctg_id/ctg_update',function(req, res){
+    cn.query(`select * from ctg where id=?`,[`${req.params.ctg_id}`],function(err, results){
+        if(err) throw err;
+        content = `
+        <form action="/ctg/${req.params.ctg_id}/ctg_updating" method="post">
+            <p>
+                
+                <input type="text" name="new_category" value="${results[0].category}">
+            </p>
+            <input type="submit" >
+        </form>
+        `;
+        cn.query('select * from ctg', 
+        function (error, ctg_results) {
+            if (error) throw error;
+            let ctg_list =tpl.ctg_list(ctg_results);
+            res.end(tpl.template(ctg_list, '', '', '', content));
+        })
+    })
+})
+
+router.post('/:ctg_id/ctg_updating',function(req, res){
+    
+
+    let new_category = req.body.new_category;
+
+    //sanitize HTML
+    const clean_category = sanitizeHtml(new_category, {
+        allowedTags: [ 'b', 'i', 'em', 'strong', 'a','br' ],
+    });
+
+    //ctg update
+    cn.query('update ctg set category=? where id=?',[`${clean_category}`, `${req.params.ctg_id}`], function(err){
+        if (err) throw err;
+        res.writeHead(302, {Location: `/ctg/${req.params.ctg_id}`});
+        res.end();
+    })    
+})
+
+
+router.post('/:ctg_id/ctg_deleting',function(req, res){
+    cn.query('delete from ctg where id=?',[`${req.params.ctg_id}`])
+    cn.query('delete from total where ctg_id=?',[`${req.params.ctg_id}`],function(err){
+        if (err) throw err;
+        res.writeHead(302, {Location: `/`});
+        res.end();
+    })
+})
 
 
 //read date & content
 router.get('/:ctg_id', function (req,res){
-    
     let button = tpl.button(req);
     cn.query('select * from ctg', 
     function (error, ctg_results) {
@@ -166,6 +247,9 @@ router.post('/:ctg_id/content/:content_id/cont_deleting',function(req, res){
     res.writeHead(302, {Location: `/ctg/${req.params.ctg_id}`});
     res.end();
 })
+
+
+
 
 
 module.exports=router;
