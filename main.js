5ab01d4b41ba20f4424806a438d02dd06aaec21a
bappurii <bappul_tt@naver.com>
2021-08-02 22:36:53 +0900

    Express-Session stored in mySQL

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-// const url = require('url');
-// const http = require('http');
-// const qs = require('querystring');
-
 //my module
 const tpl = require('./lib/template');
 const cn=require('./lib/mysql');
@@ -13,14 +9,44 @@ const sanitizeHtml = require('sanitize-html');
 //Express
 const express = require('express');
 const bodyParser = require('body-parser');
-const compression = require('compression')
-const helmet = require('helmet')
+const compression = require('compression');
+const helmet = require('helmet');
 
-const app = express()
 
-app.use(bodyParser.urlencoded({ extended: false }))
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
 app.use(helmet());
+
+
+
+//session
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+const options = {
+	host: 'localhost',
+	user: 'root',
+	password: '4321',
+	database: 'diary1'
+};
+const sessionStore = new MySQLStore(options);
+app.set('trust proxy', 1);
+app.use(session({
+    secret: 'secret1@3@',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { 
+        secure: true,
+        httpOnly: true,
+        maxAge: 1000*60*60*24*90
+    },
+    store : sessionStore
+}));
+
+
+
 
 
 app.get('/favicon.ico', function (req, res) {
